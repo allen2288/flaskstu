97b4297144805fb2e7131359eca9301da2be841b
allen2288 <867030007@qq.com>
2017-02-09 17:42:41 +0800

    first

--- flaskstu.py
+++ flaskstu.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/<id>')
+def user(id):
+    return "I am %s" % id
 
 if __name__ == '__main__':
     app.run()
