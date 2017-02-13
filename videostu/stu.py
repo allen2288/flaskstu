eb91de25aa80a41cef7c3584e0ce2690ddc00a01
allen <18565621323@139.com>
2017-02-13 08:27:11 +0800

    create directory

---
+++ videostu/stu.py
@@ -0,0 +1,11 @@
+class person:
+    xue='blood'
+    def __init__(self,name):
+        self.name=name
+class alex:
+    xue='no xin no fei'
+
+p1 = person('allen')
+p2 = person('zhaoxinqiang')
+print p1.name
+print p2.name
